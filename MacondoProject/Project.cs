7ca576df786d7c87124e8e5755cbe1884d288b87
Melollo4 <melvin.regular.human@gmail.com>
2026-07-30 15:25:01 +0200

    v6

--- MacondoProject/Project.cs
+++ MacondoProject/Project.cs
@@ -6,8 +6,9 @@ class Program
     static void Main()
     {
 
-        // Season Variables
+        // Season / Effect Variables
         string currentSeason = "Summer";
+        string specialEffects = "";
 
         // Status Variables
         int day = 1;
@@ -38,20 +39,20 @@ class Program
         {
             Console.Clear();
 
-            /*
+            
             Console.WriteLine("                    __   __    ________     ______    ______     ___   __    ______     ______                          ");
-            Console.WriteLine("                   |  \_/  |  |   __   |   /  ____|  /  __  \   |   \ |  |  |  __  \   /  __  \                         ");
-            Console.WriteLine("                   | |\_/| |  |  |__|  |  |  |____  |  |__|  |  |  |\\|  |  | |__|  | |  |__|  |                        ");
-            Console.WriteLine("                   |_|   |_|  |__|  |__|   \______|  \______/   |__| \___|  |______/   \______/                         ");
+            Console.WriteLine("                   |  \\_/  |  |   __   |   /  ____|  /  __  \\   |   \\ |  |  |  __  \\   /  __  \\                         ");
+            Console.WriteLine("                   | |\\_/| |  |  |__|  |  |  |____  |  |__|  |  |  |\\\\|  |  | |__|  | |  |__|  |                        ");
+            Console.WriteLine("                   |_|   |_|  |__|  |__|   \\______|  \\______/   |__| \\___|  |______/   \\______/                         ");
             Console.WriteLine("");
             Console.WriteLine("      ______     _______    ________    ________    __         _______    __   __    _______    __    __    ________    ");
-            Console.WriteLine("     /  ____\   |  ___ _|  |__    __|  |__    __|  |  |       |  ___ _|  |  \_/  |  |  ___ _|  |   \ |  |  |__    __|   ");
-            Console.WriteLine("     \____  \   |  ___|_      |  |        |  |     |  |____   |  ___|_   | |\_/| |  |  ___|_   |  |\\|  |     |  |      ");
-            Console.WriteLine("     \______/   |_______|     |__|        |__|     |_______|  |_______|  |_|   |_|  |_______|  |__| \___|     |__|      ");            
+            Console.WriteLine("     /  ____\\   |  ___ _|  |__    __|  |__    __|  |  |       |  ___ _|  |  \\_/  |  |  ___ _|  |   \\ |  |  |__    __|   ");
+            Console.WriteLine("     \\____  \\   |  ___|_      |  |        |  |     |  |____   |  ___|_   | |\\_/| |  |  ___|_   |  |\\\\|  |     |  |      ");
+            Console.WriteLine("     \\______/   |_______|     |__|        |__|     |_______|  |_______|  |_|   |_|  |_______|  |__| \\___|     |__|      ");            
             Console.WriteLine("");
-            */
+            
 
-            Console.WriteLine("                                              MACONDO SETTLEMENT                                     v4.7.2");
+            Console.WriteLine("                                                         v6");
             Console.WriteLine("");
             Console.WriteLine("1 - Play");
             Console.WriteLine("2 - Rules");
@@ -102,7 +103,7 @@ class Program
             Console.Clear();
             Console.WriteLine("Status:     Months Survived: " + day + " | Settlers: " + settlers);
             Console.WriteLine("Resources:  Food: " + food + " | Ingredients: " + ingredients + " | Materials: " + materials);
-            Console.WriteLine("Structures: Available Houses: " + availableHouses);
+            Console.WriteLine("Structures: Available Homes: " + availableHouses);
             Console.WriteLine("");
             Console.WriteLine("Actions left today: " + actionsLeft);
             Console.WriteLine("");
@@ -116,7 +117,7 @@ class Program
             }
             else if (currentSeason == "Fall")
             {
-                Console.Write(": It's raining like crazy! Homes need more materials to build");
+                Console.Write(": It's raining like crazy! Homes need more materials to build.");
             }
             else if (currentSeason == "Winter")
             {
@@ -127,55 +128,58 @@ class Program
                 Console.Write(": The wonderful beginning is here! Forage and Scavange for extra stock!");
             }
             Console.WriteLine("");
+            if (currentLevel > 2)
+            {
+                Console.WriteLine("Special Effect(s): " + specialEffects);   
+            }
             Console.WriteLine("");
-            Console.WriteLine("Choices:");
-            Console.WriteLine("                                     BASICS");
+            Console.WriteLine("World Table:");
 
-            Console.WriteLine("1  - End Day");
+            Console.WriteLine(" ");
 
-            if (currentSeason == "Spring")
-            {
-                Console.WriteLine("2  - Forage Ingredients     [-1 Action]      [+4 Ingredients]                         ");
-            }
-            else
-            {
-                Console.WriteLine("2  - Forage Ingredients     [-1 Action]      [+3 Ingredients]                         ");
-            }
+                Console.WriteLine("3  - Cook Food              [Settlers: " + settlers + "] [Actions Left: " + actionsLeft + "] [Ingredients: " + ingredients + "] [Food: " + food + "] [Materials: " + materials + "] [Available Homes: " + availableHouses + "] [$: " + money + "]");
 
-            Console.WriteLine("3  - Cook Food              [-2 Ingredients] [+1 Food]                                ");
 
             if (currentSeason == "Spring")
             {
-                Console.WriteLine("4  - Scavenge Materials     [-1 Action]      [+3 Materials]                           ");
+                Console.WriteLine("2  - Forage Ingredients                    [-1]               [+4]                                                                       ");
             }
             else
             {
-                Console.WriteLine("4  - Scavenge Materials     [-1 Action]      [+2 Materials]                           ");
+                Console.WriteLine("2  - Forage Ingredients                    [-1]               [+3]                                                                       ");
+            }
+
+                Console.WriteLine("3  - Cook Food                                                [-2]               [+1]                                                    ");
+
+            if (currentSeason == "Spring")
+            {
+                Console.WriteLine("4  - Scavenge Materials                    [-1]                                             [+3]                                         ");
+            }
+            else
+            {
+                Console.WriteLine("4  - Scavenge Materials                    [-1]                                             [+2]                                         ");
             }
 
             if (currentSeason == "Fall")
             {
-                Console.WriteLine("5  - Build a Home           [-1 Action]      [-6 Materials]       [+1 Available Home]");
+                Console.WriteLine("5  - Build a Home                          [-1]                                             [-6]            [+1]                         ");
             }
             else
             {
-                Console.WriteLine("5  - Build a Home           [-1 Action]      [-5 Materials]       [+1 Available Home]");
+                Console.WriteLine("5  - Build a Home                          [-1]                                             [-5]            [+1]                         ");
             }
 
-            Console.WriteLine("6  - Reproduce              [-1 Action]      [-1 Available Home]  [+1 Settler]        ");
+                Console.WriteLine("6  - Reproduce              [+1]           [-1]                                                             [-1]                         ");
 
-            if (currentLevel > 0)
-            {
-                Console.WriteLine("");
-            Console.WriteLine("                                     MARKET");
-                Console.WriteLine("7  - Sell Food              [-" + currentLevel + " Food]        [+" + (currentLevel +1) + " $]                                   ");
-                Console.WriteLine("8  - Buy Food               [-2 $]           [+1 Food]                                ");
-            }
             if (currentLevel > 1)
             {
-                Console.WriteLine("9  - Sell Material          [-" + (currentLevel -1) + " Materials]        [+" + (currentLevel *2) + " $]                                   ");
-                Console.WriteLine("10 - Buy Material           [-3 $]           [+1 Material]                                ");
-                Console.WriteLine("11 - Buy a Home             [-15 $]          [+1 Available Home]                   ");
+                Console.WriteLine("7  - Sell Ingredients                                         [-" + currentLevel + "]                                                                   [+" + (currentLevel +1) + "]");
+                Console.WriteLine("8  - Buy Food                                                                    [+1]                                            [-6]                                ");
+            }
+            if (currentLevel > 2)
+            {
+                Console.WriteLine("9  - Sell Material                                                                          [-" + (currentLevel -1) + "]                                 [+" + ((currentLevel -1) *2) + "]");
+                Console.WriteLine("10 - Buy a Home                                                                                             [+1]                 [-15]");
             }
 
             // Read Player Input
@@ -246,32 +250,26 @@ class Program
                 actionsLeft --;
                 settlers ++;
             }
-            // Sells Food if Player is a higher Level than 0 and has enough Food to Sell
-            else if (input == "7" && currentLevel > 0 && food >= currentLevel)
+            // Sells Ingredients if Player is a higher Level than 1 and has enough Food to Sell
+            else if (input == "7" && currentLevel > 1 && food >= currentLevel)
             {
                 food -= currentLevel;
                 money += currentLevel +1;
             }
-            // Buys Food if Player is a higher Level than 0 and has at least 2 Money
-            else if (input == "8" && currentLevel > 0 && money > 1)
+            // Buys Food if Player is a higher Level than 1 and has at least 2 Money
+            else if (input == "8" && currentLevel > 1 && money > 1)
             {
                 money -= 2;
                 food ++;
             }
-            // Sells Materials if player is a higher Level than 1 and has enough Materials to Sell
-            else if (input == "9" && currentLevel > 1 && materials >= (currentLevel -1))
+            // Sells Materials if player is a higher Level than 2 and has enough Materials to Sell
+            else if (input == "9" && currentLevel > 2 && materials >= (currentLevel -1))
             {
                 materials -= currentLevel -1;
                 money += currentLevel *2;
             }
-            // Buys Materials if Player is a higher Level than 1 and has at least 3 Money
-            else if (input == "10" && currentLevel > 1 && money > 2)
-            {
-                money -= 3;
-                materials ++;
-            }
             // Buys an Available House if Player has a higher Level than 1 and has at least 15 Money
-            else if (input == "11" && currentLevel > 1 && money > 14)
+            else if (input == "10" && currentLevel > 1 && money > 14)
             {
                 money -= 15;
                 availableHouses ++;
@@ -285,6 +283,38 @@ class Program
             // Day Ends
             if (actionsLeft == -1)
             {
+
+                if (currentLevel == 0)
+                {
+                    if (settlers > 4)
+                    {
+                        currentLevel ++;
+                            currentGoal = "Level 2: Have 20 Food";
+                     }
+                }
+                if (currentLevel == 1)
+                {
+                    if (food > 19)
+                    {
+                        currentLevel ++;
+                        currentGoal = "Level 3: Have $100";
+                    }
+                }
+                if (currentLevel == 2)
+                {
+                    if (money > 99)
+                    {
+                        currentLevel ++;
+                        currentGoal = "Enjoy life :]";
+                    }
+                }
+                // (Survive the next Year)
+
+                if (specialEffects == "Heatwave: You'll loose 2 extra Food at the end of the Month")
+                {
+                    food -= 2;
+                }
+
                 // clear screen, show food eaten and if survived (remove one settler if not enough food, isAlive = false if 0 settlers), set food to 0 if negative, reset actions, increase day by one)
                 Console.Clear();
                 Console.WriteLine("Food: " + food);
@@ -316,58 +346,75 @@ class Program
                 if (isAlive)
                 {
                     Console.WriteLine(food + " Food remain.");
+                    Console.WriteLine("");
                     day ++;
                     actionsLeft = settlers;
                     seasonalCountdown --;
+
                     if (seasonalCountdown == 0)
                     {
 
                         if (currentSeason == "Summer")
                         {
                             currentSeason = "Fall";
+                            Console.WriteLine("It is now Fall.");
+                            
                         }
                         else if (currentSeason == "Fall")
                         {
                             currentSeason = "Winter";
+                            Console.WriteLine("It is now Winter.");
+                            
                         }
                         else if (currentSeason == "Winter")
                         {
                             currentSeason = "Spring";
+                            Console.WriteLine("It is now Spring.");
+                            
                         }
                         else if (currentSeason == "Spring")
                         {
                             currentSeason = "Summer";
+                            Console.WriteLine("It is now Summer.");
+                            
                         }
 
                         seasonalCountdown = 4;
 
                     }
-                    
-                    if (currentLevel == 0)
+
+                    if (currentLevel > 2)
                     {
-                        if (settlers > 4)
+                        Random random = new Random();
+                        int rand = random.Next(1, 31);
+
+                        if (rand == 5)
                         {
-                            currentLevel ++;
-                            currentGoal = "Level 2: Have 20 Food";
+                            specialEffects = "Heatwave: You'll loose 2 extra Food at the end of the Month";
+                            Console.WriteLine("");
+                            Console.WriteLine(specialEffects);
+                        }
+                        else if (rand == 15)
+                        {
+                            specialEffects = "Perfect Rain: You brought back an extra 5 Ingredients this Month";
+                            ingredients += 5;
+                            Console.WriteLine("");
+                            Console.WriteLine(specialEffects);
+                        }
+                        else if (rand == 25)
+                        {
+                            specialEffects = "Encounter: A traveler stumbles upon your settlement and joins.";
+                            settlers ++;
+                            Console.WriteLine("");
+                            Console.WriteLine(specialEffects);
+                        }
+                        else
+                        {
+                            specialEffects = "";
                         }
                     }
-                    if (currentLevel == 1)
-                    {
-                        if (food > 19)
-                        {
-                            currentLevel ++;
-                            currentGoal = "Level 3: Have $100";
-                        }
-                    }
-                    if (currentLevel == 2)
-                    {
-                        if (money > 99)
-                        {
-                            currentLevel ++;
-                            currentGoal = "Enjoy life :]";
-                        }
-                    }
-                    // (Survive the next Year)
+
+                    Console.WriteLine("");
 
                     Console.Write("[Press ENTER to Continue]");
                     Console.ReadLine();
